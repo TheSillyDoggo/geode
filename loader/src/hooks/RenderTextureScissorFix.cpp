1b1e99a1545ebedeb97f5c9de8592d946c1b239c
TheSillyDoggo <a@a.com>
2025-12-07 20:35:13 +1100

    mac hopefully

--- loader/src/hooks/RenderTextureScissorFix.cpp
+++ loader/src/hooks/RenderTextureScissorFix.cpp
@@ -5,15 +5,23 @@ using namespace geode::prelude;
 
 class $modify (CCEGLView)
 {
-    virtual void setScissorInPoints(float x, float y, float w, float h)
+    virtual void setScissorInPoints(float x , float y , float w , float h)
     {
         if (CCKeyboardDispatcher::get()->getShiftKeyPressed())
         {
             GLint viewport[4];
             glGetIntegerv(GL_VIEWPORT, viewport);
 
-            float multWidth = viewport[2] / (m_obScreenSize.width * m_fFrameZoomFactor);
-            float multHeight = viewport[3] / (m_obScreenSize.height * m_fFrameZoomFactor);
+            #ifdef GEODE_IS_WINDOWS
+            float zoomFactor = m_fFrameZoomFactor;
+            #endif
+
+            #ifdef GEODE_IS_MAC
+            float zoomFactor = CCEAGLView::sharedEGLView()->frameZoomFactor_;
+            #endif
+
+            float multWidth = viewport[2] / (m_obScreenSize.width * zoomFactor);
+            float multHeight = viewport[3] / (m_obScreenSize.height * zoomFactor);
 
             x *= multWidth;
             y *= multHeight;
